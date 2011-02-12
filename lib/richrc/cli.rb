dba059612a9608a0808492c67e0db5bd2ba5e7cc
BigCat <miaout17@gmail.com>
2011-02-12 23:15:34 +0800

    customize

--- lib/richrc/cli.rb
+++ lib/richrc/cli.rb
@@ -1,10 +1,13 @@
 module Richrc
   class CLI < Thor
+
     include Thor::Actions
-    desc 'config', 'generate .richrc config file in current path'
-    def config
+
+    desc 'customize', 'generate .richrc config file in current path'
+    def customize
       default_config = File.expand_path(File.join(__FILE__, "../../../config/default.rb"))
       create_file(".richrc", File.read(default_config))
     end
+
   end
 end
